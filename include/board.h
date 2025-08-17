c7287e44862192b5e2b7935cd9f97f9c8a49e18a
Cheng Guo <cheng.guo.work@gmail.com>
2025-08-17 15:40:10 +0800

    Move boilerplate code to dedicated file.

---
+++ include/board.h
@@ -0,0 +1,24 @@
+#ifndef __BOARD_H
+#define __BOARD_H
+
+#include "stm32f1xx_hal.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Initialize HAL, clocks and minimal board peripherals used by examples
+void Board_Init(void);
+
+// Expose error handler (used by HAL callbacks)
+void Error_Handler(void);
+
+// System tick and clock setup used internally by the board
+void SystemClock_Config(void);
+void SysTick_Handler(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __BOARD_H
